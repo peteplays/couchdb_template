8e702aa3727392bc88a4e8b184672ed3188c9ab8
Peter Schnabel <Peter@Peters-MacBook-Pro.local>
2016-04-18 21:05:44 -0400

    updated readme and dbs

--- deleteDB.js
+++ deleteDB.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-var add_new_db = 'pete12345';
+var add_new_db = 'test';
 
 request.del('http://foo:bar@127.0.0.1:5984/'+add_new_db, function (error, response, body) {
   if(error) {
